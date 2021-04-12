01b291a11fd92fe7f3e201161bc83d2328e0a387
VaduvaAlexandraMihaela <56955424+VaduvaAlexandraMihaela@users.noreply.github.com>
2021-04-12 23:31:13 +0300

    done

---
+++ PracticeTestProjecgt/Data/McMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticeTestProjecgt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeTestProjecgt.Data
+{
+    public class McMovieContext : DbContext
+    {
+        public McMovieContext(DbContextOptions<McMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
